a33a24992f8d5e013de713ce81a45f8ada753ebd
Chris Hall <chall8908@gmail.com>
2017-12-21 00:47:56 -0600

    Base styles defined
    Still a bit jank with the linters and whatnot, but I have a kinda okay
    baseline to build on.  Probably going to have to do some digging around
    to find better layouts and shit...

--- app/frontend/layouts/application.js
+++ app/frontend/layouts/application.js
@@ -1,1 +1,6 @@
 import "./application.css";
+/* eslint-disable */
+import "fontawesome-pro-5.0.2/web-fonts-with-css/css/fontawesome-all.min.css";
+import "fontawesome-pro-5.0.2/svg-with-js/css/fa-svg-with-js.css";
+import "fontawesome-pro-5.0.2/svg-with-js/js/fontawesome-all.min.js";
+/* eslint-enable */
