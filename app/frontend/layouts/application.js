7ee43268a0a5213034b8ce400a7f67b951612181
Chris Hall <chall8908@gmail.com>
2018-02-23 16:39:39 -0600

    Update dependencies

--- app/frontend/layouts/application.js
+++ app/frontend/layouts/application.js
@@ -1,12 +1,11 @@
-// eslint-disable-next-line
+import "normalize.css";
+import "tomorrow-css/css/tomorrow_night.css";
 import Rails from "rails-ujs";
 
-import "./application.css";
-/* eslint-disable */
 import "fontawesome-pro-5.0.2/web-fonts-with-css/css/fontawesome-all.min.css";
 import "fontawesome-pro-5.0.2/svg-with-js/css/fa-svg-with-js.css";
-import "fontawesome-pro-5.0.2/svg-with-js/js/fontawesome-all.min.js";
-/* eslint-enable */
+import "fontawesome-pro-5.0.2/svg-with-js/js/fontawesome-all.min";
+import "./application.css";
 
 Rails.start();
 
