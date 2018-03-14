36f60d78fc4c4d3d8a04843ddaa91d33c76fac9a
Chris Hall <chall8908@gmail.com>
2018-03-14 17:52:41 -0500

    Add stimulus

--- app/frontend/layouts/application.js
+++ app/frontend/layouts/application.js
@@ -1,11 +1,17 @@
 import "normalize.css";
 import "tomorrow-css/css/tomorrow_night.css";
 import Rails from "rails-ujs";
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 import "fontawesome-pro-5.0.2/web-fonts-with-css/css/fontawesome-all.min.css";
 import "fontawesome-pro-5.0.2/svg-with-js/css/fa-svg-with-js.css";
 import "fontawesome-pro-5.0.2/svg-with-js/js/fontawesome-all.min";
 import "./application.css";
+
+const application = Application.start();
+const context = require.context(".", true, /.js$/);
+application.load(definitionsFromContext(context));
 
 Rails.start();
 
