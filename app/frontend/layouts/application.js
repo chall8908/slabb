1488bd1496c4c81311ec5cdb9b17f684ce82a31e
Chris Hall <chall8908@gmail.com>
2018-03-16 16:53:07 -0500

    Add Turbolinks

--- app/frontend/layouts/application.js
+++ app/frontend/layouts/application.js
@@ -1,5 +1,6 @@
 import "normalize.css";
 import "tomorrow-css/css/tomorrow_night.css";
+import Turbolinks from "turbolinks";
 import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
@@ -8,11 +9,13 @@ import "fontawesome-pro-5.0.2/svg-with-js/css/fa-svg-with-js.css";
 import "fontawesome-pro-5.0.2/svg-with-js/js/fontawesome-all.min";
 import "./application.css";
 
-const application = Application.start();
-const context = require.context(".", true, /.js$/);
-application.load(definitionsFromContext(context));
-
 // Remove the no JS warning if they have JS turned on.  :D
 window.onload = function() {
   document.querySelector(".alert--notice-no-js").remove();
+
+  Turbolinks.start();
+
+  const application = Application.start();
+  const context = require.context(".", true, /.js$/);
+  application.load(definitionsFromContext(context));
 };
